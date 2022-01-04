23b358b2a55ae384a166222ac2e71d2fb2f88f56
Rana Swami <ranaswam@gmail.com>
2022-01-04 19:20:03 +0530

    PredicateExample updated!

--- functionalInterface/PredicateExample.java
+++ functionalInterface/PredicateExample.java
@@ -12,21 +12,21 @@ public class PredicateExample {
 
     public static void predicateAnd(){
 
-        System.out.println("Predicate and result is: "+p1.and(p2).test(10));
-        System.out.println("Predicate and result is: "+p1.and(p2).test(9));
+        System.out.println("Predicate and result is: "+p1.and(p2).test(10));//predicate chaining
+        System.out.println("Predicate and result is: "+p1.and(p2).test(9));//predicate chaining
 
     }
 
     public static void predicateOr(){
 
-        System.out.println("Predicate OR result is: "+p1.or(p2).test(10));
-        System.out.println("Predicate OR result is: "+p1.or(p2).test(8));
+        System.out.println("Predicate OR result is: "+p1.or(p2).test(10));//predicate chaining
+        System.out.println("Predicate OR result is: "+p1.or(p2).test(8));//predicate chaining
 
     }
 
     public static void predicateNegate(){
 
-        System.out.println("Predicate Negate result is: "+p1.or(p2).negate().test(10));
+        System.out.println("Predicate Negate result is: "+p1.or(p2).negate().test(10));//predicate chaining
 
     }
 
